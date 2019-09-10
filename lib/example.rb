cc4f31954d963e3ba68e77b69acb641a37c94805
Arjun Nair <nair.arjun47@yahoo.com>
2019-09-10 06:07:37 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,17 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def hello_world
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+hello_world
